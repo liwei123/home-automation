8adfea9d5f49de4045363872948908257d6d16e9
liwei123 <liwei13193@hotmail.com>
2018-03-17 17:41:50 +0800

    added module and component, have not renamed password and date

--- home-automation-angular/src/app/app.component.ts
+++ home-automation-angular/src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { UserService } from './services/user.service';
+import User from './models/user.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  // title = 'app';
+
+   constructor(
+    //Private todoservice will be injected into the component by Angular Dependency Injector
+    private userService: UserService
+  ) { }
+
+  //Declaring the new todo Object and initilizing it
+  public newUser: User = new User()
+
+  //An Empty list for the visible users list
+  usersList: User[];
+  editUsers: User[] = [];
+
+  ngOnInit(): void {
+
+    //At component initialization the 
+    this.userService.getUsers()
+      .subscribe(users => {
+        //assign the todolist property to the proper http response
+        this.usersList = users
+        console.log(users)
+      })
+  }
+
+  create() {
+    this.userService.createUser(this.newUser)
+      .subscribe((res) => {
+        this.usersList.push(res.data)
+        this.newUser = new User()
+      })
+  }
+
+  editUser(user: User) {
+    console.log(user)
+    if(this.usersList.includes(user)){
+      if(!this.editUsers.includes(user)){
+        this.editUsers.push(user)
+      }else{
+        this.editUsers.splice(this.editUsers.indexOf(user), 1)
+        this.userService.editUser(user).subscribe(res => {
+          console.log('Update Succesful')
+        }, err => {
+          this.editUser(user)
+          console.error('Update Unsuccesful')
+        })
+      }
+    }
+  }
+
+  submitTodo(event, user:User){
+    if(event.keyCode ==13){
+      this.editUser(user)
+    }
+  }
 }
