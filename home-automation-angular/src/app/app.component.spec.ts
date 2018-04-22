8bb97156940c315c0ac93c8a08588d71e7eedb35
liwei123 <liwei13193@hotmail.com>
2018-04-22 13:49:36 +0800

    add a reference link for spec

--- home-automation-angular/src/app/app.component.spec.ts
+++ home-automation-angular/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+// for reference : https://angular.io/guide/testing
 import { TestBed, async } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
